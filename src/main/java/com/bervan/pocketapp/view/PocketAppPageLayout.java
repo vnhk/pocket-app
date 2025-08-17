cc2696f86ca53b54432a44737c1cb7e7f74e6843
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:35:07 +0200

    New Navigation menu icons

--- src/main/java/com/bervan/pocketapp/view/PocketAppPageLayout.java
+++ src/main/java/com/bervan/pocketapp/view/PocketAppPageLayout.java
@@ -8,8 +8,8 @@ public final class PocketAppPageLayout extends MenuNavigationComponent {
     public PocketAppPageLayout(String currentRouteName) {
         super(currentRouteName);
 
-        addButtonIfVisible(menuButtonsRow, AbstractPocketView.ROUTE_NAME, "Pockets", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractAllPocketItemsView.ROUTE_NAME, "Pocket Items", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractPocketView.ROUTE_NAME, "Pockets", VaadinIcon.CLIPBOARD.create());
+        addButtonIfVisible(menuButtonsRow, AbstractAllPocketItemsView.ROUTE_NAME, "Pocket Items", VaadinIcon.CLIPBOARD_TEXT.create());
 
         add(menuButtonsRow);
 
