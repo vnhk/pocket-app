e84b5f04585e1f95c6020bd3282e315bdd529c2e
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:27:18 +0200

    New Navigation menu styling

--- src/main/java/com/bervan/pocketapp/view/PocketAppPageLayout.java
+++ src/main/java/com/bervan/pocketapp/view/PocketAppPageLayout.java
@@ -1,14 +1,15 @@
 package com.bervan.pocketapp.view;
 
 import com.bervan.common.MenuNavigationComponent;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 public final class PocketAppPageLayout extends MenuNavigationComponent {
 
     public PocketAppPageLayout(String currentRouteName) {
         super(currentRouteName);
 
-        addButtonIfVisible(menuButtonsRow, AbstractPocketView.ROUTE_NAME, "Pockets");
-        addButtonIfVisible(menuButtonsRow, AbstractAllPocketItemsView.ROUTE_NAME, "Pocket Items");
+        addButtonIfVisible(menuButtonsRow, AbstractPocketView.ROUTE_NAME, "Pockets", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractAllPocketItemsView.ROUTE_NAME, "Pocket Items", VaadinIcon.HOME.create());
 
         add(menuButtonsRow);
 
