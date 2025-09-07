07ceaf4e003598ee6d61598ee081bd4d5d27cbe9
berluk12@gmail.com <berluk12@gmail.com>
2025-09-07 17:30:50 +0200

    Refactor package structure for better modularity
    Moved classes into more appropriate packages like `view` and `component` for improved organization and clarity. Removed the unused `AbstractTableAction` enum. Updated import paths and references accordingly.

--- src/main/java/com/bervan/pocketapp/view/AbstractAllPocketItemsView.java
+++ src/main/java/com/bervan/pocketapp/view/AbstractAllPocketItemsView.java
@@ -1,6 +1,6 @@
 package com.bervan.pocketapp.view;
 
-import com.bervan.common.AbstractBervanTableView;
+import com.bervan.common.view.AbstractBervanTableView;
 import com.bervan.core.model.BervanLogger;
 import com.bervan.pocketapp.pocket.Pocket;
 import com.bervan.pocketapp.pocket.PocketService;
